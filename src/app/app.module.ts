2299c6c9ea86e3671e249c3e6fb56ea14079e6fb
iromashko <iromashko@me.com>
2021-02-19 21:25:11 +0300

    part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'contactmanager',
+    loadChildren: () =>
+      import('./contactmanager/contactmanager.module').then(
+        (m) => m.ContactmanagerModule
+      ),
+  },
+  {
     path: 'demo',
     loadChildren: () => import('./demo/demo.module').then((m) => m.DemoModule),
   },
   {
     path: '**',
-    redirectTo: 'demo',
+    redirectTo: 'contactmanager',
   },
 ];
 
